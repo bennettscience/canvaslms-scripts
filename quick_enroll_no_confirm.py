3e305c82f2997033ad3eb717a7b22be231f5aa4c
bennettscience <brian.bennett2@gmail.com>
2020-12-12 22:22:56 -0500

    Enroll a user with no confirmation required

---
+++ quick_enroll_no_confirm.py
@@ -0,0 +1,16 @@
+from config import PROD_URL, PROD_KEY
+from canvasapi import Canvas
+
+canvas = Canvas(PROD_URL, PROD_KEY)
+
+course = canvas.get_course(12345)
+# specify sections if you want to
+# wrap the enrollment object in a loop for eac section
+# sections = [59592, 59600, 59611] 
+
+enrollment = {
+    "notify": False,
+    "enrollment_state": "active",
+}
+
+course.enroll_user(000000, "TeacherEnrollment", enrollment=enrollment)
